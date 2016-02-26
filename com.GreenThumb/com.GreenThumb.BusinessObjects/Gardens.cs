38ec8a0a860dc563a8694e50eb9f9d2f8d99d335
unknown <NH229U05@nh229c05.csd.bit>
2016-02-26 12:26:04 -0600

    Luke Frahm added DTOs

---
+++ com.GreenThumb/com.GreenThumb.BusinessObjects/Gardens.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.GreenThumb.BusinessObjects
+{
+    public class Garden
+    {
+        public int GardenID { get; set; }
+        public int GroupID { get; set; }
+        public int UserID { get; set; }
+        public string GardenDescription { get; set; }
+        public string GardenRegion { get; set; }
+
+        public Garden() { }
+
+        public Garden(int gardenID, int groupID, int userID, string gardenDescription, string  gardenRegion)
+        {
+            GardenID = gardenID;
+            GroupID = groupID;
+            UserID = userID;
+            GardenDescription = gardenDescription;
+            GardenRegion = gardenRegion;
+        }
+    }
+}
