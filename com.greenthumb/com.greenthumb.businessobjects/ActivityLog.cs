1b578ea665edf562825a39302805a48a7098a4cc
rhettallen <14rallen@gmail.com>
2016-02-28 20:34:28 -0600

    Fixed duplicate classes

--- com.greenthumb/com.greenthumb.businessobjects/ActivityLog.cs
+++ com.greenthumb/com.greenthumb.businessobjects/ActivityLog.cs
@@ -1,6 +1,4 @@
-﻿// Added by Poonam Dubey on 02/27/2016
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +8,7 @@ namespace com.GreenThumb.BusinessObjects
 {
     public class ActivityLog
     {
+        // Added by Poonam Dubey on 02/27/2016
         public int ActivityLogID { get; set; }
         public int UserID { get; set; }
         public DateTime ActivityDate { get; set; }
