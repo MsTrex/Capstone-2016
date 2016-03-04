22b5006ba3e6ca744d831980a92e6d12dd64dc7e
TrentCullinan9412 <trent.cullinan9412@gmail.com>
2016-03-04 10:40:14 -0600

    Added org promotes user to group leader

---
+++ com.GreenThumb/com.GreenThumb.WPF_Presentation/OrgGroupLeaderManager.xaml.cs
@@ -0,0 +1,211 @@
+﻿using com.GreenThumb.BusinessLogic;
+using com.GreenThumb.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace com.GreenThumb.WPF_Presentation
+{
+    /// <summary>
+    /// Interaction logic for OrgGroupLeaderManager.xaml
+    /// Created By: Trent Cullinan 02/24/2016
+    /// </summary>
+    public partial class OrgGroupLeaderManager : Window
+    {
+        private AccessToken accessToken;
+        private OrgUserManager orgUserManager;
+        private OrgRequestsManager orgRequestManager;
+
+        /// <summary>
+        /// Initialize constructor and confirm access for an organization.
+        /// Created By: Trent Cullinan 02/24/2016
+        /// </summary>
+        /// <param name="accessToken">Confirm user is valid to use method.</param>
+        /// <param name="organization">Organization to be referenced.</param>
+        public OrgGroupLeaderManager(AccessToken accessToken, Organization organization)
+        {
+            InitializeComponent();
+
+            try
+            {
+                orgUserManager = new OrgUserManager(accessToken, organization);
+                orgRequestManager = new OrgRequestsManager(accessToken, organization);
+
+                this.accessToken = accessToken;
+            }
+            catch (Exception ex)
+            {
+                lblResponseMessage.Content = "Error: " + ex.Message;
+            }
+
+            BindOrganizationUsers();
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void BindOrganizationUsers()
+        {
+            try
+            {
+                dgrdUsers.ItemsSource = orgUserManager.FetchOrgUsers(this.accessToken);
+                dgrdUsers.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                lblResponseMessage.Content = "Error: " + ex.Message;
+            }
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void dgrdUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblResponseMessage.Content = string.Empty;
+            btnActionOne.Content = ((GroupMember)dgrdUsers.SelectedItem).Leader ? 
+                "Demote" : "Promote";
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void btnActionOne_Click(object sender, RoutedEventArgs e)
+        {
+            if (tiOrgUsers.IsSelected)
+            {
+                GroupMember user = (GroupMember)dgrdUsers.SelectedItem;
+
+                try
+                {
+                    lblResponseMessage.Content = orgUserManager.ChangeUserLeader(this.accessToken, user) ?
+                        "User changed!" : "Sorry, selected user could not be changed.";
+
+                    BindOrganizationUsers();
+
+                    btnActionOne.Content = user.Leader ?
+                        "Demote" : "Promote";
+                }
+                catch (Exception ex)
+                {
+                    lblResponseMessage.Content = "Error: " + ex.Message;
+                }
+            }
+            else if (tiRequests.IsSelected)
+            {
+                GroupLeaderRequest request = (GroupLeaderRequest)dgrdRequests.SelectedItem;
+
+                if (null != request)
+                {
+                    try
+                    {
+                        lblResponseMessage.Content = orgRequestManager.ApproveRequest(
+                            this.accessToken, request, orgUserManager.RetrieveGroupMember(accessToken, request.User)) ?
+                            "Request Approved" : "Unable to approve";
+
+                        BindLeaderRequests();
+                    }
+                    catch (Exception ex)
+                    {
+                        lblResponseMessage.Content = "Error: " + ex.Message;
+                    }
+                }
+                else
+                {
+                    lblResponseMessage.Content = "Error: Please select a request.";
+                }
+            }
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void btnActionTwo_Click(object sender, RoutedEventArgs e)
+        {
+            if (tiOrgUsers.IsSelected)
+            {
+                GroupMember user = (GroupMember)dgrdUsers.SelectedItem;
+
+                try
+                {
+                    OrgManageUserGroups orgManageUserGroups = new OrgManageUserGroups(
+                        this.accessToken, this.orgUserManager, user);
+
+                    this.Hide();
+                    orgManageUserGroups.ShowDialog();
+                    this.Show();
+                }
+                catch (Exception ex)
+                {
+                    lblResponseMessage.Content = "Error: " + ex.Message;
+                }
+            }
+            else if (tiRequests.IsSelected)
+            {
+                GroupLeaderRequest request = (GroupLeaderRequest)dgrdRequests.SelectedItem;
+
+                if (null != request)
+                {
+                    try
+                    {
+                        lblResponseMessage.Content = orgRequestManager.DeclineRequest(accessToken, request) ?
+                            "Request Declined" : "Unable to decline";
+
+                        BindLeaderRequests();
+                    }
+                    catch (Exception ex)
+                    {
+                        lblResponseMessage.Content = "Error: " + ex.Message;
+                    }
+                }
+                else
+                {
+                    lblResponseMessage.Content = "Error: Please select a request.";
+                }
+            }
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void BindLeaderRequests() 
+        {
+            try
+            {
+                dgrdRequests.ItemsSource = orgRequestManager.FetchOrgRequests(accessToken);
+                dgrdRequests.Items.Refresh();
+            }
+            catch (Exception ex)
+            {
+                lblResponseMessage.Content = "Error: " + ex.Message;
+            }
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void tcScreens_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lblResponseMessage.Content = string.Empty;
+
+            if (tiOrgUsers.IsSelected)
+            {
+                BindOrganizationUsers();
+
+                btnActionOne.Content = "Promote";
+                btnActionTwo.Content = "Manage";
+            }
+            else if (tiRequests.IsSelected)
+            {
+                BindLeaderRequests();
+
+                btnActionOne.Content = "Approve";
+                btnActionTwo.Content = "Decline";
+            }
+        }
+
+        // Created By: Trent Cullinan 02/24/2016
+        private void dgrdRequests_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            e.Handled = true; // Required or else events will boil up
+        }
+    }
+}
