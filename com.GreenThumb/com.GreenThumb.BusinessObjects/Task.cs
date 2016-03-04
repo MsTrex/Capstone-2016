eb5cedf0b0151a1348da623235fbb14a33f6c6cc
tglisch <tglisch@gmail.com>
2016-03-04 09:57:53 -0600

    Added Documentation to team Taylors Classes

--- com.GreenThumb/com.GreenThumb.BusinessObjects/Task.cs
+++ com.GreenThumb/com.GreenThumb.BusinessObjects/Task.cs
@@ -10,6 +10,13 @@ namespace com.GreenThumb.BusinessObjects
 {
     public class Task
     {
+        /// <summary>
+        /// Author: Poonam
+        /// Data Transfer Object to represent a Task from the
+        /// Database
+        /// 
+        /// Added 3/3 By Trevor Glisch
+        /// </summary>
         public int TaskID { get; set; }
         public string TaskDescription { get; set; }
         public bool TaskActive { get; set; }
