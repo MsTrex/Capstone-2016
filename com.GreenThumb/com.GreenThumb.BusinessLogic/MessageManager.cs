5943ee0d1b69193d6832e539fcf8da6619b7b1c8
unknown <NH229U26@nh229c26.csd.bit>
2016-03-04 10:37:26 -0600

    Comitting message functionality

---
+++ com.GreenThumb/com.GreenThumb.BusinessLogic/MessageManager.cs
@@ -0,0 +1,29 @@
+﻿using com.GreenThumb.BusinessObjects;
+using com.GreenThumb.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.GreenThumb.BusinessLogic
+{
+    /// <summary>
+    /// Added by Sara Nanke on 03/04/2016
+    /// This Class manages message objects to send to the view
+    /// </summary>
+    public class MessageManager
+    {
+        /// <summary>
+        /// Added by Sara Nanke on 03/04/2016
+        /// This method retrieves messages
+        /// </summary>
+        public List<Message> GetUserMessages()
+        {
+            List<Message> messages = new List<Message>();
+            messages = MessageAccessor.fetchAdminMessages();
+
+            return messages;
+        }
+    }
+}
