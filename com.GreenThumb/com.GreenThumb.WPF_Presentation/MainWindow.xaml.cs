dadecca97de8ed5e8c0fd6dc60b72e90e45c4a30
Ryan Taylor <ryant2355@gmail.com>
2016-03-07 10:47:31 -0600

    Ryan Taylor fixed the login code, it should work with a user who has roles

--- com.GreenThumb/com.GreenThumb.WPF_Presentation/MainWindow.xaml.cs
+++ com.GreenThumb/com.GreenThumb.WPF_Presentation/MainWindow.xaml.cs
@@ -21,17 +21,64 @@ namespace com.GreenThumb.WPF_Presentation
     /// </summary>
     public partial class MainWindow : Window
     {
-        private AccessToken _accessToken;
+        private AccessToken _accessToken = null;
+        Login _login;
+
         public MainWindow()
         {
             InitializeComponent();
             mainFrame.NavigationService.Navigate(new Uri("HomeContent.xaml", UriKind.Relative));
         }
 
+        /// <summary>
+        /// Ryan Taylor
+        /// Created: 03/04/2016
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Login_Click(object sender, RoutedEventArgs e)
         {
-            Login _login = new Login();
-            _login.ShowDialog();
+            if (null == _accessToken)
+            {
+                _login = new Login();
+                _login.AccessTokenCreatedEvent += setAccessToken;
+                if (_login.ShowDialog() == true && _accessToken != null) // login succeeded
+                {
+                    this.btnLogin.Header = "Log Out";
+                    // this is where we will set the initial privilages based on roles
+
+                }
+                else
+                {
+                    // clear the access token reference
+                    _accessToken = null;
+                    MessageBox.Show("Login Failed.");
+                }
+            }
+            else // somebody is already logged in
+            {
+                _accessToken = null;
+                this.btnLogin.Header = "Log In";
+                // change things back to default here.
+            }
+            
+            
+            
+        }
+
+        /// <summary>
+        /// Ryan Taylor
+        /// Created: 03/05/2016
+        /// A method to subscribe to the login event that sets access token.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="a">The access token being sent</param>
+        private void setAccessToken(object sender, AccessToken a)
+        {
+            if (sender == _login)
+            {
+                this._accessToken = a;
+            }
         }
 
         private void NewUser_Click(object sender, RoutedEventArgs e)
