25eaf3d406c00ee2036b4829b96a21efc13eacd8
Trent Cullinan <trent.cullinan9412@gmail.com>
2016-04-01 00:48:33 -0500

    [Web]User can view current groups and leave them

---
+++ com.GreenThumb/com.GreenThumb.MVC/Models/UserGroupsModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace com.GreenThumb.MVC.Models
+{
+    /// <summary>
+    /// ViewModel for a user viewing basic information about a group.
+    /// 
+    /// Created By: Trent Cullinan 02/31/2016
+    /// </summary>
+    public class UserGroupViewModel
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public string LeaderUserName { get; set; }
+        public string LeaderEmail { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
