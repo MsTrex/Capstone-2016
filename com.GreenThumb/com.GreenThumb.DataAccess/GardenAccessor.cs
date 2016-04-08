a18a77a78922de6635359c1d79415a04dfab8f62
Kristine Johnson <Kristine Johnson>
2016-04-08 16:48:48 -0500

    added creation of a new garden mvc

--- com.GreenThumb/com.GreenThumb.DataAccess/GardenAccessor.cs
+++ com.GreenThumb/com.GreenThumb.DataAccess/GardenAccessor.cs
@@ -51,6 +51,45 @@ namespace com.GreenThumb.DataAccess
             return updated;
         }
 
+        /// <summary>
+        /// Created by: Kristine
+        /// </summary>
+        /// <param name="userID"></param>
+        /// <param name="garden"></param>
+        /// <returns></returns>
+        public static bool AddGarden(Garden garden)
+        {
+            var conn = DBConnection.GetDBConnection();
+            var query = "Gardens.spInsertGardens";
+            var cmd = new SqlCommand(query, conn);
+
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@GroupID", garden.GroupID);
+            cmd.Parameters.AddWithValue("@UserID", garden.UserID);
+            cmd.Parameters.AddWithValue("@GardenDescription", garden.GardenDescription);
+            cmd.Parameters.AddWithValue("@GardenRegion", DBNull.Value);
+            bool updated = false;
+
+            try
+            {
+                conn.Open();
+                if (cmd.ExecuteNonQuery() == 1)
+                {
+                    updated = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return updated;
+        }
+
 
         /// <summary>
         /// Accessor function fetch all gardens : Poonam Dubey  (20th March 2016)
