85b1627613819f031d78986d773f69c0d7b7d8e7
Kristine Johnson <Kristine Johnson>
2016-04-22 10:19:05 -0500

    added to create garden, with map to select region

--- com.GreenThumb/com.GreenThumb.MVC/Models/GardenCreationViewModel.cs
+++ com.GreenThumb/com.GreenThumb.MVC/Models/GardenCreationViewModel.cs
@@ -19,6 +19,8 @@ namespace com.GreenThumb.MVC.Models
 
         [HiddenInput(DisplayValue = false)]
         public int GroupID { get; set; }
+
+        public int RegionID { get; set; }
         
     }
 }
