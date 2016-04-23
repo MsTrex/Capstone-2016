98982ee7886913f2c51a64bdad44113dc265b124
chrissheehan777 <chris.sheehan777@gmail.com>
2016-04-22 19:40:41 -0500

    db update

--- Database/GreenThumbDB.sql
+++ Database/GreenThumbDB.sql
@@ -4699,23 +4699,6 @@ go
 
 --created by chris schwebach 4-15-16
 
-CREATE PROCEDURE [Gardens].[spSelectTasksForGarden](
-	@GardenID int
-)
-AS
-BEGIN
-
-	--SELECT TaskID, Description , CONVERT(VARCHAR(20),DateAssigned) 'AssignedOn',   ISNULL(CONVERT(VARCHAR(20),DateCompleted),'') 'CompletedOn', ISNULL(AUU.FirstName + ' ' + AUU.LastName,'') 'AssignedTo', AU.FirstName + ' ' + AU.LastName 'AssignedBy' , UserNotes, GT.Active , ISNULL(GT.AssignedTo, 0)  
-
-	SELECT TaskID, Description , CONVERT(VARCHAR(20),DateAssigned) 'AssignedOn',   ISNULL(CONVERT(VARCHAR(20),DateCompleted),'') 'CompletedOn', ISNULL(AUU.FirstName + ' ' + AUU.LastName,'') 'AssignedTo', AU.FirstName + ' ' + AU.LastName 'AssignedBy' , UserNotes, GT.Active  
-
-	FROM Gardens.Tasks GT
-	INNER JOIN Admin.Users AU 
-	ON GT.AssignedFrom = AU.UserID  
-	LEFT JOIN  Admin.Users AUU ON GT.AssignedTo = AUU.UserID  
-	WHERE GT.GardenID = @GardenID AND GT.Active = 1
-END;
-go
 
 
 
@@ -4783,6 +4766,24 @@ BEGIN
    
 END
 GO
+
+--- Created By : Chris Schwebach 04/15.2016 and Modified by Poonam dubey 04/22.2016 
+
+--created by ponam dubey 4-22-16
+CREATE PROCEDURE [Gardens].[spSelectTasksForGarden](
+	@GardenID int
+)
+As
+BEGIN
+	SELECT TaskID, Description , CONVERT(VARCHAR(20),DateAssigned) 'AssignedOn',   ISNULL(CONVERT(VARCHAR(20),DateCompleted),'') 'CompletedOn', ISNULL(AUU.FirstName + ' ' + AUU.LastName,'') 'AssignedTo', AU.FirstName + ' ' + AU.LastName 'AssignedBy' , UserNotes, GT.Active , ISNULL(GT.AssignedTo,0) 
+	FROM Gardens.Tasks GT
+	INNER JOIN Admin.Users AU 
+	ON GT.AssignedFrom = AU.UserID  
+	LEFT JOIN  Admin.Users AUU ON GT.AssignedTo = AUU.UserID  
+	WHERE GT.GardenID = @GardenID AND GT.Active = 1
+END;
+go
+
 ------------------------------------------
 -----------Gardens.WorkLogs---------------
 ------------------------------------------
