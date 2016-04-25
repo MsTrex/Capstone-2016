56575399a8f96cb853b26950eb3ab89aff0fb8a7
Trent Cullinan <trent.cullinan9412@gmail.com>
2016-04-24 19:53:17 -0500

    [Web]Clean up POST garden creation with Region Id

--- com.GreenThumb/com.GreenThumb.DataAccess/GardenAccessor.cs
+++ com.GreenThumb/com.GreenThumb.DataAccess/GardenAccessor.cs
@@ -68,7 +68,7 @@ namespace com.GreenThumb.DataAccess
             cmd.Parameters.AddWithValue("@GardenName", garden.GardenName);
             cmd.Parameters.AddWithValue("@UserID", garden.UserID);
             cmd.Parameters.AddWithValue("@GardenDescription", garden.GardenDescription);
-            cmd.Parameters.AddWithValue("@GardenRegion", DBNull.Value);
+            cmd.Parameters.AddWithValue("@GardenRegion", garden.GardenRegion);
             bool updated = false;
 
             try
