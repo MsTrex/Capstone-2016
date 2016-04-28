3e73cd7674f803f0fca5a21e837d126f87393e28
Ryan Taylor <ryant2355@gmail.com>
2016-04-28 16:35:01 -0500

    Luke Frahm Added Announcement feature

---
+++ com.GreenThumb/com.GreenThumb.BusinessLogic/Interfaces/IAnnouncementManager.cs
@@ -0,0 +1,18 @@
+﻿using com.GreenThumb.BusinessObjects;
+using System;
+using System.Collections.Generic;
+
+namespace com.GreenThumb.BusinessLogic.Interfaces
+{
+    /// <summary>
+    /// Created By: Luke Frahm 4/22/2016
+    /// AnnouncementManager Interface for the Buisness Logic Layer
+    /// </summary>
+    public interface IAnnouncementManager
+    {
+        bool CreateAnnouncement(Announcements announcement);
+        List<Announcements> GetAnnouncementsByGroupID(int groupID);
+        List<Announcements> GetAnnouncementsByGroupIDTop10(int userID);
+        bool UpdateAnnouncement(Announcements announcement);
+    }
+}
